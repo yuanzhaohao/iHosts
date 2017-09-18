365e7316f2af157b1fe333a5f5be962d6a8a5a79
yuanzhaohao <yuanzhaohao@163.com>
2017-09-18 19:58:21 +0800

    add emitter.js

--- src/index.js
+++ src/index.js
@@ -2,10 +2,20 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.less';
 
+import emitter from './lib/emitter';
+
+emitter.on('emitterTest', function(params) {
+  console.log(params);
+});
+
 import Pannel from './components/Panel';
 import Editor from './components/Editor';
 
 function App() {
+  console.log(process);
+  console.log(process.platform);
+
+
   return (
     <div className="container">
       <Pannel />
