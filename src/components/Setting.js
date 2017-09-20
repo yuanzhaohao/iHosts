8835bb28b73c1d7377184112b72cdf3519ca1271
yuanzhaohao <yuanzhaohao@163.com>
2017-09-20 23:06:06 +0800

    rebuilkd

--- src/components/Setting.js
+++ src/components/Setting.js
@@ -1,12 +1,22 @@
 'use strict';
 
 import React from 'react';
-import { Icon } from 'antd';
+import { Icon, Modal } from 'antd';
 import './setting.less';
 
 export default class Setting extends React.Component {
   constructor(props) {
     super(props);
+
+    this.state = {
+      visible: false
+    };
+  }
+
+  onAddClick = () => {
+    this.setState({
+      visible: true
+    });
   }
 
   render() {
@@ -15,9 +25,19 @@ export default class Setting extends React.Component {
         <div className="setting-item">
           <Icon type="setting" />
         </div>
-        <div className="setting-item">
+        <div className="setting-item" onClick={this.onAddClick}>
           <Icon type="plus" />
         </div>
+        <Modal
+          title="Basic Modal"
+          visible={this.state.visible}
+          onOk={this.handleOk}
+          onCancel={this.handleCancel}
+        >
+          <p>Some contents...</p>
+          <p>Some contents...</p>
+          <p>Some contents...</p>
+        </Modal>
       </div>
     );
   }
